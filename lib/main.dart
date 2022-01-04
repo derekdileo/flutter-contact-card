92483c4c3e8aa275c92f424aed8b75e020226388
Derek DiLeo <derek@Dereks-Mac-mini.local>
2022-01-04 14:37:10 -0500

    Fix LaunchImage and Assets.xcassets as well as add url launcher flutter package to enable link to website.

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,128 @@
+import 'dart:convert';
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
+  runApp(MyApp());
+}
+
+// Need StatelessWidget for hot reload!
+class MyApp extends StatelessWidget {
+  String _url = 'https://derekdileo.github.io/';
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.teal,
-        body: Container(),
+        backgroundColor: Colors.white,
+        //appBar: AppBar(backgroundColor: Colors.deepOrange, title: Text('')),
+        body: SafeArea(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              // Picture
+              CircleAvatar(
+                radius: 50,
+                backgroundImage: AssetImage('images/derek.JPG'),
+              ),
+
+              // Name
+              Text(
+                'Derek DiLeo',
+                style: TextStyle(
+                  fontFamily: 'Pacifico',
+                  fontSize: 40.0,
+                  color: Colors.black,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+
+              // Title
+              Text(
+                'JUNIOR DEVELOPER',
+                style: TextStyle(
+                  fontFamily: 'Source Sans Pro',
+                  fontSize: 20.0,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.black,
+                ),
+              ),
+
+              // Line Separator
+              SizedBox(
+                height: 20.0,
+                width: 150.0,
+                child: Divider(
+                  color: Colors.black,
+                ),
+              ),
+
+              // Phone
+              Card(
+                color: Colors.white,
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                child: ListTile(
+                  leading: Icon(
+                    Icons.phone,
+                    color: Colors.black,
+                  ),
+                  title: Text(
+                    '(781) 316-6719',
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontFamily: 'Source Sans Pro',
+                      fontSize: 20.0,
+                    ),
+                  ),
+                ),
+              ),
+
+              // Email
+              Card(
+                color: Colors.white,
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                child: ListTile(
+                  leading: Icon(
+                    Icons.email,
+                    color: Colors.black,
+                  ),
+                  title: Text(
+                    'derekfdileo@gmail.com',
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontFamily: 'Source Sans Pro',
+                      fontSize: 20.0,
+                    ),
+                  ),
+                ),
+              ),
+
+              // Link to website
+              Card(
+                color: Colors.white,
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                child: IconButton(
+                    icon: Icon(
+                      Icons.link,
+                      color: Colors.black,
+                    ),
+                    onPressed: () {
+                      launch(_url);
+                      print('link pressed');
+                    }),
+              ),
+
+              // Link retry
+              Card(
+                color: Colors.white,
+                child: ElevatedButton(),
+              ),
+            ],
+          ),
+        ),
       ),
-    ),
-  );
+    );
+  }
 }
